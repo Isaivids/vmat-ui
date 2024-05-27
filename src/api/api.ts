c90c7acb729d4cb84d15588150a4a15ce7a28eae
devisaivids <devisaivids@gmail.com>
2024-05-27 11:55:41 +0530

    api ch

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const apiCall = axios.create({
-    // baseURL: `https://vmat-api.vercel.app/api`,
-    baseURL: `http://localhost:5000/api`,
+    baseURL: `https://vmat-api.vercel.app/api`,
+    // baseURL: `http://localhost:5000/api`,
 })
