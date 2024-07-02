21f96dabeb8a5d048d652f578fd66b5ed87ab7de
devisaivids <devisaivids@gmail.com>
2024-07-02 16:14:25 +0530

    date change

--- src/components/calender/CommonDatePicker.tsx
+++ src/components/calender/CommonDatePicker.tsx
@@ -16,13 +16,19 @@ const CommonDatePicker: React.FC<CommonDatePickerProps> = ({
 }) => {
   const value = rowData[field.field];
   const isValidDate = value && !isNaN(new Date(value).getTime());
+  const getYesterdayDate = () => {
+    const today = new Date();
+    const yesterday = new Date(today);
+    yesterday.setDate(today.getDate() - 1);
+    return yesterday;
+  };
   return (
     <Calendar
       value={isValidDate ? new Date(value) : null}
       onChange={(e) => onDateChange(e, rowData._id, field.field)}
       disabled={rowData._id !== selectedRowId}
       style={{ width: "150px" }}
-      minDate={new Date()}
+      minDate={getYesterdayDate()}
       dateFormat="dd/mm/yy"
       placeholder="Select Date"
     />
