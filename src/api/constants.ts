549bf5116d4b4cff64228753fd1ddd70780035f8
devisaivids <devisaivids@gmail.com>
2024-07-24 18:25:28 +0530

    www

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -158,7 +158,7 @@ export const getTCPDoc = () => {
     { field: "ats.date", header: "Date" },
     { field: "ats.transname", header: "Transport Name" },
     { field: "ats.trucknumber", header: "Truck Number" },
-    { field: "ats.transcrossing", header: "Trans Crossing" },
+    { field: "transcrossing", header: "Trans Crossing" },
     { field: "others", header: "Others" },
     { field: "remarks", header: "Remarks" },
     { field: "total", header: "Total" },
@@ -296,7 +296,7 @@ export const pageName = [
   { type: 8, name: 'Truck Details' },
 ]
 
-export const initialrows = 10;
-export const paginationRows = [10,20,30];
+export const initialrows = 50;
+export const paginationRows = [50,100,150];
 
 export const totalColumns = ['total','transadvtotruck','advanceamount','finaltotaltotruckowner','pending','tyrasporterpaidamt']
