550374bf05927821edbcc28483e07935fdfaaaa4
isaivids <isaivids2020@gmail.com>
2026-04-18 14:26:18 +0530

    fg

--- src/pages/Invest/Invest.tsx
+++ src/pages/Invest/Invest.tsx
@@ -102,11 +102,11 @@ const Invest = () => {
       <div className="flex justify-content-between mx-4 mb-3">
         <div className="flex align-items-center">
           <label className="mr-2">Total To Be Paid</label>
-          <h3 className="mr-2 text-red-500">₹ {formatCurrency(totalToBePaid)}</h3>
+          <h3 className="mr-2 text-red-500">{formatCurrency(totalToBePaid)}</h3>
         </div>
         <div className="flex align-items-center">
           <label className="mr-2">Total Outstanding</label>
-          <h3 className="mr-2 text-red-500">₹ {formatCurrency(totalOutStanding)}</h3>
+          <h3 className="mr-2 text-red-500">{formatCurrency(totalOutStanding)}</h3>
         </div>
       </div>
 
